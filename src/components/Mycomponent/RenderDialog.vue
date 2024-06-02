14b4811797978d9bec9c6396d9a81f58c3f8c7cb
zhanqifan <2697433082@qq.com>
2024-06-02 23:39:13 +0800

    '日常数据静态'

---
+++ src/components/Mycomponent/RenderDialog.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+const props=defineProps<{
+    title:string
+}>()
+const isShow = ref(false)
+const sumbit =()=>{
+}
+const open =()=>{
+    console.log(props.title)
+    isShow.value=true
+}
+
+defineExpose({
+    open
+})
+</script>
+<template>
+  <el-dialog v-model="isShow" :title="title" width="500" align-center>
+    <el-divider style="height: 10px;margin:0" />
+    <slot />
+    <template #footer>
+      <div class="dialog-footer">
+        <el-button @click="isShow = false">取消</el-button>
+        <el-button type="primary" @click="sumbit"> 确认</el-button>
+      </div>
+    </template>
+  </el-dialog>
+</template>
+
+<style lang="scss" scoped></style>
--- src/views/daliyManage/actualSport.vue
+++ src/views/daliyManage/actualSport.vue
@@ -1,6 +1,28 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Plus } from '@element-plus/icons-vue'
+import RenderDialog from '@/components/Mycomponent/RenderDialog.vue';
+const dialogRef = ref()
+const addTraining = () =>{
+  dialogRef.value.open()
+}
+</script>
 <template>
-  <div>实时 运动数据</div>
+  <div class="main">
+    <el-button type="primary" :icon="Plus" class="btn" round @click="addTraining">添加训练</el-button>
+    <RenderDialog :title="'添加训练'" ref="dialogRef"> </RenderDialog>
+  </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.main{
+   height: 100vh;
+   display: flex;
+   align-items: center;
+   justify-content: center;
+   .btn{
+    width: 30vw;
+    height: 20vh;
+    font-size: 30px;
+   }
+}
+</style>
